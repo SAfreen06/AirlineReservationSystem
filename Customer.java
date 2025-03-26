bb42d2823ac3d6c1f13cf96f65fac5a216ec18d8
SAfreen06 <afreensanjana74@gmail.com>
2025-03-26 12:08:42 +0600

    moved readCustomerData to user

--- Customer.java
+++ Customer.java
@@ -61,36 +61,11 @@ public class Customer {
      * already registered, program will ask the user
      * to enter new email address to get himself register.
      */
-    public void addNewCustomer() {
-        Customer newCustomer = readCustomerInfo();
-        customerCollection.add(newCustomer);
+    public void addNewCustomer(Customer customer) {
+        customerCollection.add(customer);
     }
 
-    private Customer readCustomerInfo(){
 
-        System.out.printf("\n\n\n%60s ++++++++++++++ Welcome to the Customer Registration Portal ++++++++++++++", "");
-        Scanner read = new Scanner(System.in);
-        System.out.print("\nEnter your name :\t");
-        String name = read.nextLine();
-        System.out.print("Enter your email address :\t");
-        String email = read.nextLine();
-        while (isUniqueData(email)) {
-            System.out.println(
-                    "ERROR!!! User with the same email already exists... Use new email or login using the previous credentials....");
-            System.out.print("Enter your email address :\t");
-            email = read.nextLine();
-        }
-        System.out.print("Enter your Password :\t");
-        String password = read.nextLine();
-        System.out.print("Enter your Phone number :\t");
-        String phone = read.nextLine();
-        System.out.print("Enter your address :\t");
-        String address = read.nextLine();
-        System.out.print("Enter your age :\t");
-        int age = read.nextInt();
-
-        return new Customer(name,email,password,phone,address,age);
-    }
 
     /**
      * Returns String consisting of customers data(name, age, email etc...) for
@@ -201,7 +176,7 @@ public class Customer {
      * @param showHeader to check if we want to print ascii art for the customers'
      *                   data.
      */
-    public void displayCustomersData(boolean showHeader) {
+    public void displayCustomersData(boolean showHeader) {//remove showheader
         displayHeader();
         Iterator<Customer> iterator = customerCollection.iterator();
         int i = 0;
